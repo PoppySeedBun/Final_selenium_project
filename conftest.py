340fb8e39fcecda6e31f23a4e4a1c5e278b8aaa3
Learn <Learn@learn.com>
2019-07-14 21:31:37 +0700

    Initial files for final project

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import  Options
+
+
+def pytest_addoption(parser):		#Запрашиваем у юзера браузер и язык
+    parser.addoption('--browser', action ='store', default = "chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption("--language", action = "store", default = "en",
+    				help = "Choose language")
+
+@pytest.fixture(scope="session")
+def browser(request):				#вызываем и отдаём выбранный браузер  с выбранной локалью
+    browser_name = request.config.getoption("browser")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome": #Хром
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options = options)
+
+    elif browser_name == "firefox":	#файрфокс
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile = fp)
+
+    else:	#опечатка/неизвестный браузер - запускается хром, локаль - en-gb
+        print (f"\nBrowser {browser_name} is still not implemented, starting Chrome by default")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options = options)
+        
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
