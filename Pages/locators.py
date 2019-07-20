dbda45dbf4e632ad1e599f2b92a8873bb8f816dc
Learn <Learn@learn.com>
2019-07-20 21:48:58 +0700

    minor bug fixes

--- Pages/locators.py
+++ Pages/locators.py
@@ -2,10 +2,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
 	LOGIN_LINK = (By.CSS_SELECTOR,"#login_link")
-	
+
 class LoginPageLocators(object):
 	LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
-	LOGIN_PASSWPRD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
+	LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
 	LOGIN_RETRIEVE_PASSWORD_LINK = (By.CSS_SELECTOR, "#content_inner p a")
 	LOGIN_BUTTON = (By.CSS_SELECTOR, "button[name='login_submit']")
 
--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators   
 
 
 class LoginPage(BasePage):
@@ -8,16 +9,16 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url, "This is not login page"
+        assert "login" in self.browser.current_url, "This is not login page"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_EMAIL_FIELD), "Email field for login is not presented" 
-        assert self.is_element_present(*MainPageLocators.LOGIN_PASSWORD_FIELD), "Password field for login is not presented" 
-        assert self.is_element_present(*MainPageLocators.LOGIN_RETRIEVE_PASSWORD_LINK), "Retrieve password link is not presented"
-        assert self.is_element_present(*MainPageLocators.LOGIN_BUTTON), "Login button link is not presented"  
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_FIELD), "Email field for login is not presented" 
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD_FIELD), "Password field for login is not presented" 
+        assert self.is_element_present(*LoginPageLocators.LOGIN_RETRIEVE_PASSWORD_LINK), "Retrieve password link is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button link is not presented"  
 
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_EMAIL_FIELD), "Email field for registration is not presented" 
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_PASSWORD_FIELD), "Password field for registration is not presented" 
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_CONFIRM_PASSWORD_FIELD), "Confirm password field is not presented" 
-        assert self.is_element_present(*MainPageLocators.REGISTRATION_BUTTON), "Registration button link is not presented" 
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL_FIELD), "Email field for registration is not presented" 
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_FIELD), "Password field for registration is not presented" 
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_CONFIRM_PASSWORD_FIELD), "Confirm password field is not presented" 
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Registration button link is not presented" 
