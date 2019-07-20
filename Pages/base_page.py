6b7d0a178ac73e63edbc4920eed68553d897295d
Learn <Learn@learn.com>
2019-07-20 20:35:54 +0700

    fixes to locators,main and base page

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -9,7 +9,7 @@ class BasePage(object):
 	def open(self):
 		self.browser.get(self.url)
 
-	def is_element_present(self):
+	def is_element_present(self, how, what):
 		try:
 			self.browser.find_element(how,what)
 		except NoSuchElementException:
